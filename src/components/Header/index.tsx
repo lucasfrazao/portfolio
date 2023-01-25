7c7611e334e23688bc6ae20e8cdc62a659b04f59
Lucas Frazao <lucasfraza0@outlook.com>
2023-01-25 00:29:13 -0300

    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,52 @@
+import { Moon } from 'phosphor-react'
+import { useState, useEffect } from 'react'
+import { Container } from './styles'
+
+export function Header() {
+  const [typedText, setTypedText] = useState('')
+  const [textIndex, setTextIndex] = useState(0)
+  const text = 'hello world'
+  let intervalId: number | undefined
+
+  useEffect(() => {
+    intervalId = setInterval(() => {
+      if (textIndex < text.length) {
+        setTypedText(typedText + text[textIndex])
+        setTextIndex(textIndex + 1)
+      } else {
+        clearInterval(intervalId)
+        setTimeout(() => {
+          setTypedText('')
+          setTextIndex(0)
+          intervalId = setInterval(() => {
+            if (textIndex < text.length) {
+              setTypedText(typedText + text[textIndex])
+              setTextIndex(textIndex + 1)
+            }
+          }, 50)
+        }, 3000)
+      }
+    }, 50)
+    return () => clearInterval(intervalId)
+  }, [textIndex, text, typedText])
+
+  return (
+    <Container>
+      <div className="code">
+        <b>{`> `}</b>
+        <span>{typedText}</span>{' '}
+      </div>
+
+      <div className="menu-pages">
+        <a>About</a>
+        <a>Work</a>
+        <a>Contact</a>
+      </div>
+
+      <div className="resume">
+        <Moon size={24} color="#f9f9f9" />
+        <button>Resume</button>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+
+export const Container = styled.nav`
+  width: 100vw;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 1.5rem 2.5rem;
+
+  font-family: 'Roboto', sans-serif;
+  color: #f9f9f9;
+
+  .code {
+    width: 9rem;
+  }
+
+  .code > b {
+    color: #119da4;
+  }
+
+  .code > span {
+    font-family: 'Space Mono', monospace;
+    color: #ffc857;
+  }
+
+  .menu-pages {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1.25rem;
+  }
+
+  a {
+    border: none;
+    background: none;
+  }
+
+  .resume {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1.25rem;
+  }
+
+  .resume > svg {
+    transition: 0.2s;
+
+    :hover {
+      cursor: pointer;
+      filter: brightness(0.8);
+    }
+  }
+
+  button {
+    font-family: 'Roboto', sans-serif;
+    color: #ffc857;
+
+    padding: 0.5rem 1rem;
+
+    background: none;
+    border-radius: 0.25rem;
+    border: 1px solid #ffc857;
+
+    transition: 0.2s;
+  }
+
+  button:hover {
+    cursor: pointer;
+    filter: brightness(0.8);
+  }
+
+  @media screen and (max-width: 768px) {
+  }
+`
